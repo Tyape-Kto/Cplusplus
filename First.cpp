55184a78146d823150f49beeae048cb2d418a7ff
Tyape-Kto <55008900+Tyape-Kto@users.noreply.github.com>
2021-11-13 13:53:10 +0545

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -1,1 +1,32 @@
+#include<iostream>  
 
+// this is just a comment
+
+/* this is multi 
+line comment */
+
+int main(){
+  std::cout<< "Welcome to \n";
+  std::cout << "C++ world!";
+  return 0;
+}
+
+int main(){
+  int num1{0};
+  int num2{0};
+  
+  std:: cout << "Enter first number: ";
+  std:: cin >> num1;
+  
+  std:: cout << "Enter second number: ";
+  std:: cin >> num2;
+  
+  int sum{num1 + num2};
+  
+  std:: cout << "Total sum of number is: " << sum << std:: endl; // you can stuff num1 + num2 directly than initializing it. and this is called concatinating of strings.
+  return 0;
+}
+
+int main(){
+
+}
